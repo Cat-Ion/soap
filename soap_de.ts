0f0514749ecb99ada54d08aa18dca7803813a373
Valentin Ochs <a@0au.de>
2014-08-06 18:17:07 +0200

    Initial commit

---
+++ soap_de.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="6"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="23"/>
+        <location filename="mainwindow.ui" line="34"/>
+        <location filename="mainwindow.ui" line="45"/>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
